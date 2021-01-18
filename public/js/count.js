668e5de2b57926f8d41e7eaabcac216f01a7b279
hoseacodes <dominique11h@yahoo.com>
2021-01-17 20:52:50 -0600

    start timer

--- public/js/count.js
+++ public/js/count.js
@@ -9,3 +9,30 @@ function updateVistorCount() {
             countEl.innerHTML = res.value;
         });
 }
+
+function timer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        console.log(timer)
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+
+
+document.getElementById("start").addEventListener("click", function () {
+    console.log("click")
+    var timerMinutes = 60 * 48,
+        display = document.querySelector('#time');
+    timer(timerMinutes, display);
+})
